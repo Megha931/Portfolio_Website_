41c19fc59d13e36b2d182c3160d0f28a3c8345e4
Megha <121974968+Megha931@users.noreply.github.com>
2025-02-23 02:19:49 +0530

    Update navbar.js

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
-import contactIcon from '../../assets/contact.png'; // Adjust the path as necessary
+import contactIcon from '../../assets/contact.png'; 
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
